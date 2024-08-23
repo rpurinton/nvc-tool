3fde18db02d36460d9ddb040236ea73db5afb8f4
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-08-23 15:45:35 -0400

    Update index.php

--- index.php
+++ index.php
@@ -74,9 +74,6 @@
                     <a class="nav-link" href="#">Features</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
                     <a class="nav-link" href="#">About</a>
                 </li>
             </ul>
