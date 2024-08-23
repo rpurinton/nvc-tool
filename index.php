5e435ff40fa2eaab07ff748653489d5b8190b539
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-08-23 16:04:44 -0400

    PWA

--- index.php
+++ index.php
@@ -12,13 +12,13 @@
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">
     <link rel="manifest" href="/assets/favicon/site.webmanifest">
-    <link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg" color="#121212">
     <link rel="shortcut icon" href="/assets/favicon/favicon.ico">
     <meta name="apple-mobile-web-app-title" content="Giraffe Talk">
     <meta name="application-name" content="Giraffe Talk">
-    <meta name="msapplication-TileColor" content="#00aba9">
+    <meta name="msapplication-TileColor" content="#121212">
     <meta name="msapplication-config" content="/assets/favicon/browserconfig.xml">
-    <meta name="theme-color" content="#ffffff">
+    <meta name="theme-color" content="#121212">
 
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
@@ -33,6 +33,7 @@
 
         .navbar,
         .footer {
+            margin-top: 20px;
             background-color: #1f1f1f;
         }
 
