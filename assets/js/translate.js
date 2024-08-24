bf448b48d72fd72ccb2cbf311e0c8b61bbb25e1a
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-08-24 16:16:35 -0400

    adding translate boxes

---
+++ assets/js/translate.js
@@ -0,0 +1,33 @@
+document.getElementById('translateButton').addEventListener('click', function () {
+    var inputText = document.getElementById('inputText').value;
+    var outputText = document.getElementById('outputText');
+    var progressBar = document.getElementById('progressBar');
+
+    // Show progress bar
+    progressBar.style.display = 'block';
+    outputText.value = 'Loading...';
+
+    // Prepare the data to be sent
+    var data = JSON.stringify({ text: inputText });
+
+    // Create an AJAX request
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/translate.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+                outputText.value = response.response;
+            } else {
+                outputText.value = 'Error: Unable to translate text.';
+            }
+            // Hide progress bar
+            progressBar.style.display = 'none';
+        }
+    };
+
+    // Send the request with the input text
+    xhr.send(data);
+});
