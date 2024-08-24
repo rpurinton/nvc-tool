67c8432de4365422a5c6bc3b39d3e40959d8eafb
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-08-24 15:32:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -120,7 +120,7 @@
     <!-- Example Footer -->
     <footer class="footer mt-auto py-3">
         <div class="container">
-            <span class="text-muted">© 2023-2024 Giraffe Talk</span>
+            <span class="text-muted">©2024 Giraffe Talk</span>
         </div>
     </footer>
 
