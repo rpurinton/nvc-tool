0f8d3b46f08d0bc5f8217d66060c024ed7404f77
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-08-24 17:00:45 -0400

    Update translate.php

--- translate.php
+++ translate.php
@@ -1,6 +1,8 @@
 <?php
 $input = json_decode(file_get_contents('php://input'), true);
-$text = $input['text'];
-sleep(2);
+$text = $input['text'] ?? null;
 header('Content-Type: application/json');
+if (!$text) die(json_encode(['response' => 'Input Text is required!']));
+require_once('vendor/autoload.php');
+$openai_key = getenv('OPENAI_API_KEY') ?: die(json_encode(['response' => 'OpenAI API Key is required!']));
 echo json_encode(['response' => strtoupper($text)]);
