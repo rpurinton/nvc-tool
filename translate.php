01b931f75452266c8f4249fb105095ed22b6d36d
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-08-24 16:30:28 -0400

    Update translate.php

--- translate.php
+++ translate.php
@@ -1,3 +1,5 @@
-{
-    "response": "This is the translated text"
-}
+<?php
+$input = json_decode(file_get_contents('php://input'), true);
+$text = $input['text'];
+header('Content-Type: application/json');
+echo json_encode(['response' => strtoupper($text)]);
